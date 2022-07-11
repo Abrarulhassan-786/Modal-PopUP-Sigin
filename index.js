d541f4212c14d644a530991c234ced1590126c1d
Abrar Hassan <107148226+Abrar-Hassan-Programming@users.noreply.github.com>
2022-07-11 22:30:16 +0500

    Model PopUp sigin

---
+++ index.js
@@ -0,0 +1,25 @@
+var modal = document.getElementById("modal1")
+modal.style.display = "none"
+function model1()
+{
+    if(modal.style.display === "none")
+    {
+        console.log("hello")
+        modal.style.display = 'flex'
+    }
+    else
+    {
+        console.log("hellow")
+        modal.style.display = 'none'
+    }
+}
+/*
+var modal = document.getElementById("modal1")
+modal.style.display = "none"
+function modal1() {
+    if (modal.style.display === "none") {
+        modal.style.display = 'flex'
+    } else {
+        modal.style.display = 'none'
+    }
+}*/
